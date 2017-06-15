d95e70f3ae620621cf111d9e837a6c34f0cd03a1
Mohamed Sayed Elhagry <m.elhagry@ieee.org>
2017-06-15 14:59:47 +0200

    Elhagry
    User model that has basic data
    home controller contain index that go to index view that greeting the guest

---
+++ Party_Invitation/Party_Invitation/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Party_Invitation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            ViewBag.Greating = (DateTime.Now.Hour < 12) ? "Good Morning" : (DateTime.Now.Hour < 18) ? "Good Afternoon" : "Good Evening";
+            return View();
+        }
+	}
+}
---
+++ Party_Invitation/Party_Invitation/Models/Guest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Party_Invitation.Models
+{
+    public class Guest
+    {
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Phone")]
+        public string Phone { get; set; }
+
+        [DisplayName("Type")]
+        public bool? Admin { get; set; }
+    }
+}

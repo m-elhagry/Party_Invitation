463a212bc1b52c8ef9970fac44cb484c1f77af93
Mohamed Sayed Elhagry <m.elhagry@ieee.org>
2017-06-16 23:01:07 +0200

    Elhagry

--- Party_Invitation/Party_Invitation/Controllers/HomeController.cs
+++ Party_Invitation/Party_Invitation/Controllers/HomeController.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Party_Invitation.Models;
 namespace Party_Invitation.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Greating = (DateTime.Now.Hour < 12) ? "Good Morning" : (DateTime.Now.Hour < 18) ? "Good Afternoon" : "Good Evening";
             return View();
+        }
+        [HttpGet]
+        public ActionResult Reservation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Reservation(Guest Guest)
+        {
+            if (ModelState.IsValid == true)
+                return View("Thanks", Guest);
+            else
+                return View();
         }
 	}
 }
--- Party_Invitation/Party_Invitation/Models/Guest.cs
+++ Party_Invitation/Party_Invitation/Models/Guest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,16 +9,25 @@ namespace Party_Invitation.Models
 {
     public class Guest
     {
+        [Required(ErrorMessage = "Please enter your Name")]
         [DisplayName("Name")]
         public string Name { get; set; }
 
+
+        [Required(ErrorMessage = "Please enter your E-mail")]
         [DisplayName("Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
 
+
+        [Required(ErrorMessage = "Please enter your Phone")]
         [DisplayName("Phone")]
+        [StringLength(11, ErrorMessage = "Please enter a phone number", MinimumLength = 11)]
         public string Phone { get; set; }
 
-        [DisplayName("Type")]
-        public bool? Admin { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you will attend?")]
+        [DisplayName("WillAttend")]
+        public bool? WillAttend { get; set; }
     }
 }
